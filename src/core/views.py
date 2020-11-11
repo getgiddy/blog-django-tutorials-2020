67d93e529ea3224266d6ec1ebb2de81b1ba57edf
Gideon Anyalewechi <ganyalewechi1997@gmail.com>
2020-11-11 11:30:40 +0100

    Designed base and index template Created home view Added context from db to home view

--- src/core/views.py
+++ src/core/views.py
@@ -1,5 +1,10 @@
 from django.shortcuts import render
-from django.http import HttpResponse
+from core.models import Post
+
 
 def index(request):
-    return HttpResponse("Hello!!")
+    posts = Post.objects.all().order_by("-date_created")
+    context = {
+        "posts": posts,
+    }
+    return render(request, "index.html", context)
